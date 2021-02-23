cfe0da46c9f524ea1bf20151ba96baf4da35242b
Evgeny <evpsky@gmail.com>
2021-02-23 22:58:55 +0200

    Homework_task1 is created

---
+++ Homework_task1/Homework_task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Длина: ");
+            int sideA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ширина: ");
+            int sideB = Convert.ToInt32(Console.ReadLine());
+
+            int area = sideA * sideB;
+
+            Console.WriteLine("Площадь прямоугольника: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
