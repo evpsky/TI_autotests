0591e3b35693d9249f54626e2ce7a9c304e6bc4f
Evgeny <evpsky@gmail.com>
2021-03-04 00:10:20 +0200

    Task is added

--- Homework_task1/Homework_task1/Task1/Program.cs
+++ Homework_task1/Homework_task1/Task1/Program.cs
@@ -74,7 +74,39 @@ namespace HomeWork_1
             }
             Console.ReadLine();
 
+            const double P = 3.14;
 
+            Console.Write("Введите площадь круга: ");
+            double S1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Введите площадь квадрата: ");
+            double S2 = Convert.ToDouble(Console.ReadLine());
+
+            double Diametr = Math.Sqrt(S1 / P) * 2;
+            double Side = Math.Sqrt(S2);
+
+            if (Diametr <= Side)
+            {
+                Console.WriteLine();
+                Console.WriteLine("a) Уместится");
+            }
+
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("a) Не уместится");
+            }
+
+            double Diagonal = Side * Math.Sqrt(2);
+
+            if (Diagonal <= Diametr)
+                Console.WriteLine("б) Уместится");
+
+            else
+                Console.WriteLine("б) Не уместится");
+
+            Console.ReadKey();
         }
 
     }
